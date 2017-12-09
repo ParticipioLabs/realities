48eab39a073bb8af3f533ad5f57b8d46b5210d91
Gustav Larsson <gustav.larsson@gmail.com>
2017-12-09 21:09:07 +0100

    Add new schema definitions connected to Neo4j

--- api/src/graphql/Schema.js
+++
@@ -1,6 +0,0 @@
-import { GraphQLSchema } from 'graphql';
-import QueryType from './types/QueryType';
-
-export default new GraphQLSchema({
-  query: QueryType,
-});
--- api/src/index.js
+++ api/src/index.js
@@ -1,21 +1,72 @@
+import { makeExecutableSchema } from 'graphql-tools';
+import { neo4jgraphql } from 'neo4j-graphql-js';
 import express from 'express';
+import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
 import bodyParser from 'body-parser';
-import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
-import Schema from './graphql/Schema';
 import neo4jDriver from './db/neo4jDriver';
 
-const PORT = 3100;
+const typeDefs = `
+type Person {
+  name: String
+}
 
-const app = express();
+type Need {
+  title: String
+  fulfilledBy: [Responsibility] @relation(name: "FULFILLS", direction: "IN")
+}
 
-app.use('/graphql', bodyParser.json(), graphqlExpress(() => ({
-  schema: Schema,
-  context: {
-    neo4jSession: neo4jDriver.session(),
+type Responsibility {
+  title: String
+  fulfills: Need @relation(name: "FULFILLS", direction:"OUT")
+}
+
+type Query {
+  allPersons: [Person]
+  responsibilities: [Responsibility]
+  needs: [Need]
+}
+`;
+
+const resolvers = {
+  // root entry point to GraphQL service
+  Query: {
+    allPersons(object, params, ctx, resolveInfo) {
+      return neo4jgraphql(object, params, ctx, resolveInfo);
+    },
+    responsibilities(object, params, ctx, resolveInfo) {
+      return neo4jgraphql(object, params, ctx, resolveInfo);
+    },
+    needs(object, params, ctx, resolveInfo) {
+      return neo4jgraphql(object, params, ctx, resolveInfo);
+    },
   },
-})));
-app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' })); // Remove this to disable graphiql
+};
 
-app.listen(PORT, () => {
-  console.log(`API ready on port ${PORT}`);
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+let driver;
+
+const context = (headers) => {
+  if (!driver) {
+    driver = neo4jDriver;
+  }
+  return {
+    driver,
+    headers,
+  };
+};
+
+const PORT = 3100;
+const server = express();
+
+server.use('/graphql', bodyParser.json(), graphqlExpress(request => ({ schema, context: context(request.headers, process.env) })));
+
+server.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
+server.listen(PORT, () => {
+  console.log(`GraphQL Server is now running on http://localhost:${PORT}/graphql`);
+  console.log(`View GraphiQL at http://localhost:${PORT}/graphiql`);
 });
