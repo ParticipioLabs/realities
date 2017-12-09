52700a76c0f60b6ce02e0850d0b99b272d979e21
Erik Frisk <ef@erikfrisk.com>
2017-12-09 18:16:13 -0500

    Simplified boilerplate ui

---
+++ ui/src/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Alert } from 'reactstrap';
+
+const App = () => (
+  <div className="App">
+    <Alert color="primary">
+      We have Bootstrap 4!
+    </Alert>
+    <h1 className="App-title">Welcome to React</h1>
+  </div>
+);
+
+export default App;
