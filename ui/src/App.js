187049610600bd14417cf5bd2e5f34297edd9811
Erik Frisk <ef@erikfrisk.com>
2017-12-09 16:17:21 -0500

    Added Bootstrap 4

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Alert } from 'reactstrap';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+
+const App = () => (
+  <div className="App">
+    <Alert color="primary">
+      We have Bootstrap 4!
+    </Alert>
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header>
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  </div>
+);
 
 export default App;
--- ui/src/index.js
+++ ui/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
