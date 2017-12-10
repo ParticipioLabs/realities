e621f7819dda14ba6a273e3787bf5a5d05b95a09
Gustav Larsson <gustav.larsson@gmail.com>
2017-12-10 18:12:20 +0100

    Add link to graphql api back

--- ui/src/services/apolloClient/apolloClient.js
+++ ui/src/services/apolloClient/apolloClient.js
@@ -3,6 +3,6 @@ import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 export default new ApolloClient({
-  link: new HttpLink(),
+  link: new HttpLink({ uri: process.env.REACT_APP_GRAPHQL_ENDPOINT }),
   cache: new InMemoryCache(),
 });
