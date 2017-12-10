2bfbd16fee872526577a2846408f6e96c23992f9
Liam Kirsher <liamk@numenet.com>
2017-12-10 22:55:17 +0100

    Merged

--- ui/src/scenes/Home/components/DetailView/DetailView.js
+++ ui/src/scenes/Home/components/DetailView/DetailView.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Badge, Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button } from 'reactstrap';
 import styled from 'styled-components';
 import { RIEInput, RIETextArea } from 'riek';
 import _ from 'lodash';
 
-const Label = styled.span`
+import { Badge, Card, CardImg, CardText, CardBody, CardBlock,
+  CardTitle, CardSubtitle, Form, FormGroup, Label, Input, FormText, Row, Col, Button  } from 'reactstrap';
+
+import DependencyList from '../DependencyList';
+
+
+const BadgeLabel = styled.span`
   border-radius: 4px;
   background-color: ${props => props.color};
   padding: 5px;
@@ -14,6 +18,12 @@ const Label = styled.span`
   color: white;
   float: right;
 `;
+const RealitiesInput = styled(Input)`
+    padding-bottom: 0;
+    border: none;
+    border-bottom: 1px dotted #85bcf7;
+`
+
 
 const Title = styled(CardTitle)`
   border-bottom: 1px dotted;
@@ -64,6 +74,59 @@ class DetailView extends Component {
     return <div />;
   }
 }
+/*
+const DetailView = ({ data }) => {
+  console.log(data);
+  return (
+    <Card>
+      <CardBody>
+        <CardTitle><BadgeLabel color={data && data.__typename === 'Responsibility' ? '#843cfd' : '#00cf19'}>{data && data.__typename}</BadgeLabel> {data && data.title} </CardTitle>
+	        <Form>
+		        <FormGroup row>
+		          <Label for="guideName" sm={3}>Guide</Label>
+		          <Col sm={9}>
+		            <RealitiesInput type="text" name="guide" id="guideName" placeholder={data && data.guide.name} />
+		          </Col>
+		        </FormGroup>
+		        <FormGroup row>
+		         <Label for="realizerName" sm={3}>Realizer</Label>
+		          <Col sm={9}>
+		            <RealitiesInput type="text" name="realizer" id="realizerName" placeholder={data && data.realizer.name} />
+		          </Col>
+		        </FormGroup>
+		        <FormGroup row>
+		          <Label for="descriptionName" sm={4}>Description</Label>
+		          <Col sm={12}>
+		            <FormText name="description" id="descriptionName">
+		             {data && data.description}
+		             </FormText>
+		          </Col>
+		        </FormGroup>
+		        <FormGroup row>
+		         <Label for="deliberationName" sm={4}>Deliberation</Label>
+		          <Col sm={8}>
+		            <RealitiesInput type="text" name="deliberation" id="deliberationName" placeholder={data && data.deliberation} />
+		          </Col>
+		        </FormGroup>
+	        </Form>
+        
+			<Card>
+			  <CardBody>
+		         <Row>
+		           <Col>
+		             <DependencyList
+		              dependsOnNeeds={this.dependsOnNeeds}
+		              dependsOnResponsibilities={this.dependsOnResponsibilities}
+		            />
+		           </Col>
+		          </Row>   
+			  </CardBody>  
+			</Card>   
+	  </CardBody>
+	</Card>
+  );
+};
+*/
 
 DetailView.defaultProps = {
   data: { title: '', description: '' },
