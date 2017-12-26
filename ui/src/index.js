d66a3c24b7e57eb9a9e42578e894eae22cfd49d8
Erik Frisk <ef@erikfrisk.com>
2017-12-26 01:46:09 +0100

    Moved bootstrap theming files and added them to production webpack config

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './custom_bootstrap.scss';
+import '@/styles/custom_bootstrap.scss';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
