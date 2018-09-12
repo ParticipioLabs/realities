0c5ed5c4ecb3c1cb4e9078aac745ed8455cc6110
Erik Frisk <ef@erikfrisk.com>
2018-09-12 16:19:17 +0200

    Use template literals instead of string concatenation

--- ui/src/services/apolloClient/apolloClient.js
+++ ui/src/services/apolloClient/apolloClient.js
@@ -23,6 +23,6 @@ const authMiddleware = new ApolloLink((operation, forward) => {
 export default new ApolloClient({
   link: concat(authMiddleware, httpLink),
   cache: new InMemoryCache({
-    dataIdFromObject: object => object.__typename + ':' + object.nodeId,
+    dataIdFromObject: object => `${object.__typename}:${object.nodeId}`,
   }),
 });
