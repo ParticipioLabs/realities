94abd62fb85f869924f3e578fa3e9357c3fe675e
Erik Frisk <ef@erikfrisk.com>
2018-09-22 22:02:43 +0800

    Moved ResponsibilitiesList into a container component

---
+++ ui/src/scenes/Home/components/NeedsContainer/components/ResponsibilitiesContainer/ResponsibilitiesContainer.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import gql from 'graphql-tag';
+import { withRouter } from 'react-router-dom';
+import { Query } from 'react-apollo';
+import {
+  Container,
+  Row,
+  Col,
+} from 'reactstrap';
+import ResponsibilitiesList from './components/ResponsibilitiesList';
+
+const GET_RESPONSIBILITIES = gql`
+  {
+    responsibilities {
+      nodeId
+      title
+    }
+  }
+`;
+
+const ResponsibilitiesContainer = withRouter(({ match }) => (
+  <Query query={GET_RESPONSIBILITIES}>
+    {({ loading, error, data }) => {
+      if (loading) return 'Loading...';
+      if (error) return `Error! ${error.message}`;
+
+      return (
+        <Container fluid>
+          <Row>
+            <Col lg={3} xs={12}>
+              <ResponsibilitiesList
+                responsibilities={data.responsibilities}
+                selectedResponsibilityId={match.params.responsibilityId}
+              />
+            </Col>
+          </Row>
+        </Container>
+      );
+    }}
+  </Query>
+));
+
+ResponsibilitiesContainer.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      needId: PropTypes.string,
+      resposibilityId: PropTypes.string,
+    }),
+  }),
+};
+
+ResponsibilitiesContainer.defaultProps = {
+  match: {
+    params: {
+      needId: undefined,
+      responsibilityId: undefined,
+    },
+  },
+};
+
+export default ResponsibilitiesContainer;
