70dc94d38ad7b9f4fdc857a65621dbbe14174c2a
bob <belal.aboud@gmail.com>
2018-11-18 16:35:29 +0100

    remove hardcoded nodetype and nodeid from user graph

--- ui/src/scenes/Profile/components/UserGraph/UserGraph.js
+++ ui/src/scenes/Profile/components/UserGraph/UserGraph.js
@@ -1,25 +1,46 @@
 import React from 'react';
-import { string } from 'prop-types';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
 import LocalGraph from '@/components/LocalGraph';
+import WrappedLoader from '@/components/WrappedLoader';
 
 const CardSection = styled.div`
   margin-bottom: 1rem;
 `;
 
+const GET_PROFILE_ID_AND_TYPE = gql`
+  query($email: String!) {
+    person(email: $email) {
+      nodeId
+    }
+  }
+`;
+
 const UserGraph = ({ email }) => (
-  <CardSection>
-    <div>User Graph</div>
-    <LocalGraph
-      nodeId="0f6180cd-1c0f-4150-905a-06fbc3d9d35e"
-      nodeType="Person"
-      email={email}
-    />
-  </CardSection>
+  <Query query={GET_PROFILE_ID_AND_TYPE} variables={{ email }}>
+    {({ loading, error, data }) => {
+      if (loading) return <WrappedLoader />;
+      if (error) return `Error! ${error.message}`;
+      const node = data.person;
+      return (
+        <CardSection>
+          <div>User Graph</div>
+          <LocalGraph
+            nodeId={node.nodeId}
+            nodeType={node.__typename}
+            email={email}
+          />
+        </CardSection>
+      );
+    }}
+
+  </Query>
 );
 
 UserGraph.propTypes = {
-  email: string,
+  email: PropTypes.string,
 };
 
 UserGraph.defaultProps = {
