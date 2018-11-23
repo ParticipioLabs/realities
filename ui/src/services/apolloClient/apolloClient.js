ca58269ae18baa7027c35232320a875129acfbf2
Gustav Larsson <gustav.larsson@gmail.com>
2018-11-23 12:20:46 +0100

    Cherry pick fragment type creation

--- ui/src/services/apolloClient/apolloClient.js
+++ ui/src/services/apolloClient/apolloClient.js
@@ -2,12 +2,18 @@ import { ApolloClient } from 'apollo-client';
 import { withClientState } from 'apollo-link-state';
 import { HttpLink } from 'apollo-link-http';
 import { ApolloLink } from 'apollo-link';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import { InMemoryCache, IntrospectionFragmentMatcher } from 'apollo-cache-inmemory';
 import auth from '@/services/auth';
 import { resolvers, defaults } from './localState';
+import introspectionQueryResultData from './fragmentTypes.json';
+
+const fragmentMatcher = new IntrospectionFragmentMatcher({
+  introspectionQueryResultData,
+});
 
 const cache = new InMemoryCache({
   dataIdFromObject: object => `${object.__typename}:${object.nodeId}`,
+  fragmentMatcher,
 });
 
 const stateLink = withClientState({ cache, resolvers, defaults });
