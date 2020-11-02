df5f45d51931fdc96f7558114d0f98a7eb4da240
Jacob Karlsson <jacob.karlsson95@gmail.com>
2020-11-02 18:40:19 +0100

    Clean up unused code/comments

--- ui/src/App/App.js
+++ ui/src/App/App.js
@@ -5,15 +5,6 @@ import { ApolloProvider } from '@apollo/client';
 import apolloClient from '@/services/apolloClient';
 import AuthRoutesContainer from './components/AuthRoutesContainer';
 
-// const keycloak = Keycloak({
-//   realm: process.env.REACT_APP_KEYCLOAK_REALM,
-//   url: process.env.REACT_APP_KEYCLOAK_SERVER_URL,
-//   'ssl-required': 'external',
-//   'public-client': true,
-//   'confidential-port': 0,
-//   clientId: process.env.REACT_APP_KEYCLOAK_CLIENT,
-// });
-
 const ApolloSetup = () => {
   const { accessToken } = useAuth();
 
@@ -29,6 +20,7 @@ const App = () => (
     authority={`${process.env.REACT_APP_KEYCLOAK_SERVER_URL}/realms/${process.env.REACT_APP_KEYCLOAK_REALM}`}
     clientId={process.env.REACT_APP_KEYCLOAK_CLIENT}
     redirectUri={process.env.REACT_APP_KEYCLOAK_CALLBACK_URL}
+    autoSignIn={false}
   >
     <ApolloSetup />
   </AuthProvider>
--- ui/src/services/useAuth/useAuth.js
+++ ui/src/services/useAuth/useAuth.js
@@ -1,40 +1,13 @@
-import { useEffect } from 'react';
 import { useAuth as useOidc } from 'oidc-react';
 
 export default function useAuth() {
   const auth = useOidc();
 
-  // const [email, setEmail] = useState('');
-
-  console.log('oidc', auth);
-  console.log('session state', auth.session_state);
-
-  useEffect(() => {
-    // setEmail('test@test.com');
-    // setEmail(keycloak.tokenParsed && keycloak.tokenParsed.email);
-  });
-
-  // TODO: figure out the reactivity here
-  // https://github.com/react-keycloak/react-keycloak/issues/116
-
   return {
-    login: () => {
-      auth.signIn();
-      // keycloak.login({
-      //   redirectUri: process.env.REACT_APP_KEYCLOAK_CALLBACK_URL,
-      // });
-    },
-    logout: () => {
-      auth.signOut();
-      // keycloak.logout({
-      //   redirectUri: process.env.REACT_APP_KEYCLOAK_CALLBACK_URL,
-      // });
-    },
+    login: auth.signIn,
+    logout: auth.signOut,
     isLoggedIn: auth.userData !== null,
-    // keycloak.authenticated,
     accessToken: auth.userData && auth.userData.access_token,
-    // keycloak.token,
     email: auth.userData && auth.userData.profile.email,
-    // initialized,
   };
 }
