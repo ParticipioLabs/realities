664335f2243f6e591143cc9c38b94e2955790f91
Jacob Karlsson <jacob.karlsson95@gmail.com>
2020-11-12 15:56:37 +0100

    Switch to storing auth in localStorage

--- ui/src/App/App.js
+++ ui/src/App/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { AuthProvider } from 'oidc-react';
+import { AuthProvider, UserManager } from 'oidc-react';
+import { WebStorageStateStore } from 'oidc-client';
 import useAuth from 'services/useAuth';
 import { ApolloProvider } from '@apollo/client';
 import apolloClient from 'services/apolloClient';
@@ -15,13 +16,30 @@ const ApolloSetup = () => {
   );
 };
 
+const redirectUri = process.env.REACT_APP_KEYCLOAK_CALLBACK_URL;
+
+// we create our own userManager to store user data in localStorage instead
+// of in sessionStorage
+// https://github.com/bjerkio/oidc-react/issues/332#issuecomment-723642762
+// arguments from
+// https://github.com/bjerkio/oidc-react/blob/53c5adef53fe2603bd8507bb27fd3616fbd7e7c1/src/AuthContext.tsx#L46
+const userManager = new UserManager({
+  userStore: new WebStorageStateStore({ store: window.localStorage }),
+  authority: `${process.env.REACT_APP_KEYCLOAK_SERVER_URL}/realms/${process.env.REACT_APP_KEYCLOAK_REALM}`,
+  client_id: process.env.REACT_APP_KEYCLOAK_CLIENT,
+  redirect_uri: redirectUri,
+  silent_redirect_uri: redirectUri,
+  post_logout_redirect_uri: redirectUri,
+  response_type: 'code',
+  scope: 'openid',
+  loadUserInfo: true,
+  automaticSilentRenew: true,
+});
+
 const App = () => (
   <AuthProvider
-    authority={`${process.env.REACT_APP_KEYCLOAK_SERVER_URL}/realms/${process.env.REACT_APP_KEYCLOAK_REALM}`}
-    clientId={process.env.REACT_APP_KEYCLOAK_CLIENT}
-    redirectUri={process.env.REACT_APP_KEYCLOAK_CALLBACK_URL}
     autoSignIn={false}
-    automaticSilentRenew
+    userManager={userManager}
   >
     <ApolloSetup />
   </AuthProvider>
