e465e2440acfac922bac13afd9e32fd229838fb9
Jacob Karlsson <jacob.karlsson95@gmail.com>
2020-11-23 00:10:16 +0100

    Fix the last redirects

--- ui/src/components/Search/Search.js
+++ ui/src/components/Search/Search.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { withRouter } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import Downshift from 'downshift';
 import SearchBar from './components/SearchBar';
 import SearchResultsContainer from './components/SearchResultsContainer';
@@ -10,67 +9,60 @@ const Wrapper = styled.div`
   position: relative;
 `;
 
-const Search = withRouter(({ history }) => (
-  <Downshift
-    onChange={(node, { reset, clearSelection }) => {
-      clearSelection();
-      reset();
-      if (node) {
-        switch (node.__typename) {
-          case 'Need':
-            history.push(`/${node.nodeId}`);
-            break;
-          case 'Responsibility':
-            history.push(`/${node.fulfills.nodeId}/${node.nodeId}`);
-            break;
-          case 'Person':
-            history.push(`/profile/${node.nodeId}`);
-            break;
-          default:
-            history.push('/');
+const Search = () => {
+  const history = useHistory();
+  const { orgSlug } = useParams();
+
+  return (
+    <Downshift
+      onChange={(node, { reset, clearSelection }) => {
+        clearSelection();
+        reset();
+        if (node) {
+          switch (node.__typename) {
+            case 'Need':
+              history.push(`/${orgSlug}/${node.nodeId}`);
+              break;
+            case 'Responsibility':
+              history.push(`/${orgSlug}/${node.fulfills.nodeId}/${node.nodeId}`);
+              break;
+            case 'Person':
+              history.push(`/${orgSlug}/profile/${node.nodeId}`);
+              break;
+            default:
+              history.push(`/${orgSlug}`);
+          }
         }
-      }
-    }}
-    itemToString={i => (i && i.title) || ''}
-  >
-    {({
-      getRootProps,
-      getInputProps,
-      getMenuProps,
-      getItemProps,
-      inputValue,
-      highlightedIndex,
-      isOpen,
-      reset,
-    }) => (
-      <Wrapper {...getRootProps({ refKey: 'innerRef' })}>
-        <SearchBar
-          onClear={() => reset()}
-          {...getInputProps({ onBlur: e => e.preventDefault() })}
-        />
-        {isOpen && (
+      }}
+      itemToString={(i) => (i && i.title) || ''}
+    >
+      {({
+        getRootProps,
+        getInputProps,
+        getMenuProps,
+        getItemProps,
+        inputValue,
+        highlightedIndex,
+        isOpen,
+        reset,
+      }) => (
+        <Wrapper {...getRootProps({ refKey: 'innerRef' })}>
+          <SearchBar
+            onClear={() => reset()}
+            {...getInputProps({ onBlur: (e) => e.preventDefault() })}
+          />
+          {isOpen && (
           <SearchResultsContainer
             searchTerm={inputValue}
             getMenuProps={getMenuProps}
             getItemProps={getItemProps}
             highlightedIndex={highlightedIndex}
           />
-        )}
-      </Wrapper>
-    )}
-  </Downshift>
-));
-
-Search.propTypes = {
-  history: PropTypes.shape({
-    push: PropTypes.func,
-  }),
-};
-
-Search.defaultProps = {
-  history: {
-    push: () => null,
-  },
+          )}
+        </Wrapper>
+      )}
+    </Downshift>
+  );
 };
 
 export default Search;
