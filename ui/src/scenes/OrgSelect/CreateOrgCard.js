43ae5f77cd769c129f9061486f941af0b4b33107
Jacob Karlsson <jacob.karlsson95@gmail.com>
2020-12-27 23:10:55 +0100

    Start displaying form errors

--- ui/src/scenes/OrgSelect/CreateOrgCard.js
+++ ui/src/scenes/OrgSelect/CreateOrgCard.js
@@ -1,13 +1,24 @@
 import React, { useState } from 'react';
 import {
-  Card, CardTitle, Button, Form, FormGroup, Input,
+  Card, CardTitle, Button, Form, FormGroup, Input, FormFeedback,
 } from 'reactstrap';
 import { FaPlus } from 'react-icons/fa';
+import { gql, useMutation } from '@apollo/client';
 import { Formik } from 'formik';
 import * as yup from 'yup';
 
+const CREATE_ORG = gql`
+  mutation CreateOrgCard_createOrgMutation($name: String, $orgSlug: String) {
+    createOrg(name: $name, orgSlug: $orgSlug) {
+      orgId
+      orgSlug
+    }
+  }
+`;
+
 const CreateOrgCard = () => {
   const [creating, setCreating] = useState(false);
+  const [createOrg] = useMutation(CREATE_ORG);
 
   return (
     <Card body>
@@ -19,16 +30,24 @@ const CreateOrgCard = () => {
               name: yup.string().required('Organization name is required'),
               orgSlug: yup.string().required('Organization URL ID is required'),
             })}
-            onSubmit={(values, { resetForm }) => {
+            onSubmit={async (values, { resetForm }) => {
               // createNeed({ variables: { title: values.title } }).then(({ data }) => {
               //   resetForm();
               //   history.push(`/${params.orgSlug}/${data.createNeed.nodeId}`);
               // });
-              console.log('potato', values);
+              console.log('values', values);
+              const { data } = createOrg({
+                variables: {
+                  name: values.name,
+                  orgSlug: values.orgSlug,
+                },
+              });
+              console.log('data', data);
             }}
           >
             {({
               values,
+              errors,
               handleChange,
               handleBlur,
               handleSubmit,
@@ -45,7 +64,11 @@ const CreateOrgCard = () => {
                     disabled={isSubmitting}
                     onChange={handleChange}
                     onBlur={handleBlur}
+                    invalid={!!errors.name}
                   />
+                  <FormFeedback>{errors.name}</FormFeedback>
+                </FormGroup>
+                <FormGroup>
                   <Input
                     name="orgSlug"
                     type="text"
@@ -54,7 +77,9 @@ const CreateOrgCard = () => {
                     disabled={isSubmitting}
                     onChange={handleChange}
                     onBlur={handleBlur}
+                    invalid={!!errors.orgSlug}
                   />
+                  <FormFeedback>{errors.orgSlug}</FormFeedback>
                 </FormGroup>
                 <Button
                   size="sm"
