8859e6a9ec9e206ad8d63c7d0f7976b0213dbce7
Jacob Karlsson <jacob.karlsson95@gmail.com>
2021-01-27 16:58:13 +0100

    Add env vars for new keycloak realm

--- cypress/integration/simple_spec.js
+++ cypress/integration/simple_spec.js
@@ -1,8 +1,14 @@
 describe('Test basic functionality', () => {
   it('Create an org and a need', () => {
+    const userEmail = 'realitiestester@example.com';
+    const userPass = 'password123';
+
     cy
       .visit('localhost:3001')
       .contains('Pick an organization')
+      .root()
+      .contains('Login')
+      //.click()
   })
 })
   
