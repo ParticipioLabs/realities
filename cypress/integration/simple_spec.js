c63f43813c51514232066b7fa4d54346408f1623
Jacob Karlsson <jacob.karlsson95@gmail.com>
2021-01-28 13:53:03 +0100

    Login in tests

--- cypress/integration/simple_spec.js
+++ cypress/integration/simple_spec.js
@@ -8,7 +8,18 @@ describe('Test basic functionality', () => {
       .contains('Pick an organization')
       .root()
       .contains('Login')
-      //.click()
+      .click()
+
+      // login page on keycloak
+      // see comment in cypress.json about how this is against best practices
+      // TODO: handle cases where user is already logged in
+      // should be enough to click logout if already logged in
+      .get('#username')
+      .type(userEmail)
+      .get('#password')
+      .type(userPass)
+      .get('#kc-form-login')
+      .submit()
   })
 })
   
