5d210c6ede56cda08cb597a670056542b881e6d4
Jacob Karlsson <jacob.karlsson95@gmail.com>
2021-02-18 17:53:27 +0100

    Remove current selection for needs at the top

--- ui/src/scenes/Home/components/NeedsContainer/components/NeedsList/NeedsList.js
+++ ui/src/scenes/Home/components/NeedsContainer/components/NeedsList/NeedsList.js
@@ -1,31 +1,12 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { ListGroup, ListGroupItem } from 'reactstrap';
+import { ListGroup } from 'reactstrap';
 import _ from 'lodash';
-import colors from 'styles/colors';
-import MissingRealizersAmount from './components/MissingRealizersAmount';
 import NeedsListItem from './components/NeedsListItem';
 
 const NeedsListGroup = styled(ListGroup)`
   margin-bottom: 1rem;
-`;
-
-const SelectedNeedGroup = styled(ListGroup)`
-  margin-bottom: 0.5rem;
-`;
-
-const NeedsListGroupHeader = styled(ListGroupItem)`
-  display: flex;
-  justify-content: space-between;
-  &:focus {
-    outline: none;
-  }
-  &.active {
-    background-color: ${colors.need};
-    border-color: ${colors.need};
-    color: white;
-  }
 `;
 
 const NeedsList = ({ selectedNeedId, needs, subscribeToNeedsEvents }) => {
@@ -36,27 +17,15 @@ const NeedsList = ({ selectedNeedId, needs, subscribeToNeedsEvents }) => {
     return '';
   }], ['asc']);
   return (
-    <div>
-      {sortedNeeds.filter((need) => need.nodeId === selectedNeedId).map((need) => (
-        <SelectedNeedGroup key={1}>
-          <NeedsListGroupHeader
-            active
-          >
-            {need.title}
-            <MissingRealizersAmount need={need} />
-          </NeedsListGroupHeader>
-        </SelectedNeedGroup>
+    <NeedsListGroup>
+      {sortedNeeds.map((need) => (
+        <NeedsListItem
+          key={need.nodeId}
+          need={need}
+          isSelected={need.nodeId === selectedNeedId}
+        />
       ))}
-      <NeedsListGroup>
-        {sortedNeeds.map((need) => (
-          <NeedsListItem
-            key={need.nodeId}
-            need={need}
-            isSelected={need.nodeId === selectedNeedId}
-          />
-        ))}
-      </NeedsListGroup>
-    </div>
+    </NeedsListGroup>
   );
 };
 
